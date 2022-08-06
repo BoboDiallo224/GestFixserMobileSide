c88f909c87dc672b3c1fd31343cba30522ba7e72
unknown <bigbobo24@gmail.com>
2022-08-06 22:48:49 +0000

    Creating ExitSaleActivity

---
+++ app/src/main/java/com/example/fixsermobileapp/exit_sale/ExitSaleActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fixsermobileapp.exit_sale
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExitSaleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exit_sale)
+    }
+}
